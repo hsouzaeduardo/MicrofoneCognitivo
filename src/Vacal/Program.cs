cf2f656fa5aeeb7bd927907f92e7905e9736f8b9
Felipe Augusto <felipe.augusto@avanade.com>
2024-05-09 07:21:31 -0300

    feat: refactory and some updates

--- src/Vacal/Program.cs
+++ src/Vacal/Program.cs
@@ -1,36 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.CognitiveServices.Speech;
+﻿using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Audio;
 using Microsoft.CognitiveServices.Speech.Translation;
+using Microsoft.Extensions.Configuration;
 
 namespace Vacal
 {
     class Program
     {
-        static void Main(string[] args)
+        public static string _subscriptionKey;
+        public static string _region;
+        public static IConfiguration _configuration;
+
+        static async Task Main(string[] args)
         {
-            //SpeechToText().Wait();
-            SpeechToEng().Wait();
+            _configuration = new ConfigurationBuilder()
+                                    .AddJsonFile($"appsettings.json")
+                                    .Build();
+
+            _subscriptionKey = _configuration["SubscriptionKey"];
+            _region = _configuration["Region"];
+
+            await SpeechToText();
+            //await SpeechToTranslate();
+
+            Console.WriteLine("Pressione uma tecla para finalizar o terminal!");
         }
 
-        static async Task SpeechToEng()
+        static async Task SpeechToTranslate()
         {
             Console.WriteLine("Fale Algo");
-            var speechConfig = SpeechTranslationConfig.FromSubscription("SubscritionID"
-                , "Local da Onde Foi criado o recurso");
+            var speechConfig = SpeechTranslationConfig.FromSubscription(
+                _subscriptionKey,
+                _region);
+
             speechConfig.SpeechRecognitionLanguage = "pt-BR";
 
             //speechConfig.AddTargetLanguage("en-US");
             var langs = new List<string> { "en-US", "fr-FR", "it-IT" };
             langs.ForEach(speechConfig.AddTargetLanguage);
-            
+
             var recognizer = new TranslationRecognizer(speechConfig);
             var result = await recognizer.RecognizeOnceAsync();
+
             if (result.Reason == ResultReason.TranslatedSpeech)
             {
                 Console.WriteLine(result.Text);
+
                 foreach (var lang in result.Translations)
                 {
                     Console.WriteLine($"{lang.Key} - {lang.Value}");
@@ -40,26 +55,25 @@ namespace Vacal
             Console.ReadLine();
         }
 
-            static async Task SpeechToText()
+        static async Task SpeechToText()
         {
-            var speechConfig = SpeechConfig.FromSubscription("SubscritionID"
-                , "Local da Onde Foi criado o recurso");
+            var speechConfig = SpeechConfig.FromSubscription(_subscriptionKey, _region);
             var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
             var recognizer = new SpeechRecognizer(speechConfig, "pt-BR", audioConfig);
-            Console.WriteLine("Falai alguma coisa:");
 
-            while (true)
+            Console.WriteLine("Fale alguma coisa:");
+
+            string texto = string.Empty;
+            do
             {
                 var result = await recognizer.RecognizeOnceAsync();
-                var texto = result.Text;
+                texto = result.Text;
+
                 Console.WriteLine($"Reconhecido: {texto} ");
-                if(texto.ToLower().Contains("sair."))
-                {
-                    break;
-                }
-            }
+
+            } while (!texto.ToLower().Contains("sair"));
+
             Console.ReadLine();
         }
-
     }
 }
